11ee4441b0e179bb5c71ad7195509b80531d3478
Marcus.W <marcus.w@laggelee.se>
2024-03-12 15:15:00 +0100

    modify the shuffleTiles to never return any tiles initially in their correct position

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,14 +1,13 @@
 export const shuffleTiles = (numRows: number, numCols: number) => {
-  const totalTiles = numRows * numCols - 1;
-  const tiles = Array.from({ length: totalTiles }, (_, index) => index + 1);
-
-  tiles.push(0);
-
-  for (let i = tiles.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [tiles[i], tiles[j]] = [tiles[j], tiles[i]];
-  }
-
+  let tiles: number[];
+  do {
+    tiles = Array.from({ length: numRows * numCols }, (_, index) => index + 1);
+    for (let i = tiles.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [tiles[i], tiles[j]] = [tiles[j], tiles[i]];
+    }
+  } while (tiles.some((tile, index) => tile === index + 1));
+  tiles[tiles.length - 1] = 0;
   return tiles;
 };
 
